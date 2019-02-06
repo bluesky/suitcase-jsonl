fd6356c264e76125bccdef5160fa74a97dc98e62
gbischof <gbischof@garrett.nsls2.bnl.gov>
2019-02-06 17:31:24 -0500

    First pass

--- suitcase/json_line_delimited/__init__.py
+++ suitcase/json_line_delimited/__init__.py
@@ -2,7 +2,7 @@ import event_model
 from pathlib import Path
 import suitcase.utils
 from ._version import get_versions
-
+import json
 
 __version__ = get_versions()['version']
 del get_versions
@@ -13,25 +13,56 @@ del get_versions
 # intended to be user-facing.
 
 
-def export(gen, directory, file_prefix, **kwargs):
+def export(gen, directory, file_prefix='{uid}', **kwargs):
     serializer = Serializer(directory, file_prefix, **kwargs)
     try:
         for item in gen:
             serializer(*item)
     finally:
         serializer.close()
-
     return serializer.artifacts
 
 
 class Serializer(event_model.DocumentRouter):
-    def __init__(self, directory, file_prefix, **kwargs):
+    def __init__(self, directory, file_prefix='{uid}', **kwargs):
+
+        self._output_file = None
+        self._file_prefix = file_prefix
+        self._templated_file_prefix = ''
+        self._kwargs = kwargs
+        self._start_found = False
+
         if isinstance(directory, (str, Path)):
-            self.manager = suitcase.utils.MultiFileManager(directory)
+            self._manager = suitcase.utils.MultiFileManager(directory)
         else:
-            self.manager = directory
-        self.artifacts = self.manager._artifacts  # Expose as public.
-        ...
+            self._manager = directory
+
+        self.artifacts = self._manager.artifacts
+
+    def _check_start(self, doc):
+        if self._start_found:
+            raise RuntimeError(
+                "The serializer in suitcase-json-line-delimited expects "
+                "documents from one run only. Two `start` documents where "
+                "sent to it")
+        else:
+            self._start_found = True
+            self._templated_file_prefix = self._file_prefix.format(**doc)
+
+    def _get_file(self):
+        filename = (f'{self._templated_file_prefix}.jsonl')
+        self._output_file = self._manager.open('stream_data', filename, 'xt')
+
+    def __call__(self, name, doc):
+        if name == 'start':
+            self._check_start(doc)
+            self._get_file()
+
+        self._output_file.write("%s\n" % json.dumps(doc))
+        return name, doc
+
+    def __del__(self):
+        self.close()
 
     def close(self):
-        ...
+        self._output_file.close()
--- suitcase/json_line_delimited/tests.py
+++ suitcase/json_line_delimited/tests.py
@@ -1,2 +1,9 @@
 # Tests should generate (and then clean up) any files they need for testing. No
 # binary files should be included in the repository.
+
+from . import Serializer
+serializer = suitcase.json_line_delimited.Serializer()
+RE.subscribe(serializer)
+from bluesky.plans import scan
+from ophyd.sim import det, motor
+RE(scan([det], motor, 1, 3, 3))

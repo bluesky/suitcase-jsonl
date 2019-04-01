493ddd3fc3324f01f9138edef20ce85fbe09adaa
Maksim Rakitin <mrakitin@bnl.gov>
2019-04-01 14:07:35 -0400

    FIX: fix ophyd branch for testing; add readme content type

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     long_description=readme,
+    long_description_content_type='text/markdown',
     packages=['suitcase.jsonl', 'suitcase.jsonl.tests'],
     entry_points={
         'console_scripts': [
